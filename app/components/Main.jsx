7a93fee471f97fb8238c54d7ad0bd9b5d5052f61
Ana Priscila Alves <pva@cardio-id.com>
2016-11-28 09:46:41 +0000

    Initial commit.

---
+++ app/components/Main.jsx
@@ -0,0 +1,17 @@
+var React = require("react");
+var Nav = require("Nav")
+
+var Main = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Nav />
+                
+            </div>
+        );
+    }
+});
+
+
+
+module.exports = Main;
